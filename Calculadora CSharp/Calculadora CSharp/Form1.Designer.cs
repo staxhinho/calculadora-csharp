2bbfddf7243214815f9c3bd8e74dfbabb148f4d1
Miguel <115882736+staxhinho@users.noreply.github.com>
2022-10-25 18:54:28 +0100

    Primeiro commit

---
+++ Calculadora CSharp/Calculadora CSharp/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Calculadora_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.buttonEql = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonAC = new System.Windows.Forms.Button();
+            this.buttonNegPos = new System.Windows.Forms.Button();
+            this.buttonPrc = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button0
+            // 
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.Name = "button0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // buttonDot
+            // 
+            resources.ApplyResources(this.buttonDot, "buttonDot");
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.UseVisualStyleBackColor = true;
+            // 
+            // buttonEql
+            // 
+            resources.ApplyResources(this.buttonEql, "buttonEql");
+            this.buttonEql.Name = "buttonEql";
+            this.buttonEql.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonAdd
+            // 
+            resources.ApplyResources(this.buttonAdd, "buttonAdd");
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // buttonSub
+            // 
+            resources.ApplyResources(this.buttonSub, "buttonSub");
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // buttonMul
+            // 
+            resources.ApplyResources(this.buttonMul, "buttonMul");
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            // 
+            // buttonAC
+            // 
+            resources.ApplyResources(this.buttonAC, "buttonAC");
+            this.buttonAC.Name = "buttonAC";
+            this.buttonAC.UseVisualStyleBackColor = true;
+            // 
+            // buttonNegPos
+            // 
+            resources.ApplyResources(this.buttonNegPos, "buttonNegPos");
+            this.buttonNegPos.Name = "buttonNegPos";
+            this.buttonNegPos.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrc
+            // 
+            resources.ApplyResources(this.buttonPrc, "buttonPrc");
+            this.buttonPrc.Name = "buttonPrc";
+            this.buttonPrc.UseVisualStyleBackColor = true;
+            // 
+            // buttonDiv
+            // 
+            resources.ApplyResources(this.buttonDiv, "buttonDiv");
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            // 
+            // textResult
+            // 
+            resources.ApplyResources(this.textResult, "textResult");
+            this.textResult.BackColor = System.Drawing.Color.Black;
+            this.textResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textResult.ForeColor = System.Drawing.Color.White;
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.buttonDiv);
+            this.Controls.Add(this.buttonPrc);
+            this.Controls.Add(this.buttonNegPos);
+            this.Controls.Add(this.buttonAC);
+            this.Controls.Add(this.buttonMul);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.buttonSub);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonEql);
+            this.Controls.Add(this.buttonDot);
+            this.Controls.Add(this.button0);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button0;
+        private Button buttonDot;
+        private Button buttonEql;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button buttonAdd;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button buttonSub;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button buttonMul;
+        private Button buttonAC;
+        private Button buttonNegPos;
+        private Button buttonPrc;
+        private Button buttonDiv;
+        private TextBox textResult;
+        private Label label1;
+    }
+}
---
+++ Calculadora CSharp/Calculadora CSharp/Form1.cs
@@ -0,0 +1,60 @@
+namespace Calculadora_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textResult.Text = textResult.Text + "9";
+        }
+    }
+}
